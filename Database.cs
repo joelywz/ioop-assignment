180d77d755e549cc8da13c16f316c25c9966983e
Joel Yong <yongwenzhuo@gmail.com>
2021-10-25 09:41:58 +0800

    add Database class

---
+++ Database.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+using System.Configuration;
+
+namespace Assignment
+{
+    class Database
+    {
+
+        public static SqlConnection GetSqlConnection()
+        {
+            string value = System.Configuration.ConfigurationManager.AppSettings.Get("connectionString");
+            return new SqlConnection(value);
+        }
+
+    }
+}
