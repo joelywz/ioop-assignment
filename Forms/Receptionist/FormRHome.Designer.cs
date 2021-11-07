a5077b38159a90d4801df76636b2f8f35bd911f6
Joel Yong <yongwenzhuo@gmail.com>
2021-11-07 13:30:01 +0800

    fix receptionist home form

--- Forms/Receptionist/FormRHome.Designer.cs
+++ Forms/Receptionist/FormRHome.Designer.cs
@@ -61,10 +61,9 @@ namespace Assignment
             // 
             // btnLogOut
             // 
-            this.btnLogOut.Location = new System.Drawing.Point(597, 27);
-            this.btnLogOut.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnLogOut.Location = new System.Drawing.Point(437, 18);
             this.btnLogOut.Name = "btnLogOut";
-            this.btnLogOut.Size = new System.Drawing.Size(100, 28);
+            this.btnLogOut.Size = new System.Drawing.Size(75, 23);
             this.btnLogOut.TabIndex = 0;
             this.btnLogOut.Text = "Log out";
             this.btnLogOut.UseVisualStyleBackColor = true;
@@ -72,10 +71,9 @@ namespace Assignment
             // 
             // btnUpdateProfile
             // 
-            this.btnUpdateProfile.Location = new System.Drawing.Point(705, 27);
-            this.btnUpdateProfile.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnUpdateProfile.Location = new System.Drawing.Point(518, 18);
             this.btnUpdateProfile.Name = "btnUpdateProfile";
-            this.btnUpdateProfile.Size = new System.Drawing.Size(165, 28);
+            this.btnUpdateProfile.Size = new System.Drawing.Size(124, 23);
             this.btnUpdateProfile.TabIndex = 0;
             this.btnUpdateProfile.Text = "Update Profile";
             this.btnUpdateProfile.UseVisualStyleBackColor = true;
@@ -83,17 +81,17 @@ namespace Assignment
             // 
             // txtSearch
             // 
-            this.txtSearch.Location = new System.Drawing.Point(17, 22);
+            this.txtSearch.Location = new System.Drawing.Point(13, 22);
+            this.txtSearch.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.txtSearch.Name = "txtSearch";
-            this.txtSearch.Size = new System.Drawing.Size(210, 20);
+            this.txtSearch.Size = new System.Drawing.Size(215, 20);
             this.txtSearch.TabIndex = 1;
             // 
             // btnSearch
             // 
-            this.btnSearch.Location = new System.Drawing.Point(311, 25);
-            this.btnSearch.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnSearch.Location = new System.Drawing.Point(233, 20);
             this.btnSearch.Name = "btnSearch";
-            this.btnSearch.Size = new System.Drawing.Size(103, 28);
+            this.btnSearch.Size = new System.Drawing.Size(77, 23);
             this.btnSearch.TabIndex = 2;
             this.btnSearch.Text = "Search";
             this.btnSearch.UseVisualStyleBackColor = true;
@@ -102,9 +100,10 @@ namespace Assignment
             // lstCust
             // 
             this.lstCust.FormattingEnabled = true;
-            this.lstCust.Location = new System.Drawing.Point(20, 119);
+            this.lstCust.Location = new System.Drawing.Point(13, 105);
+            this.lstCust.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.lstCust.Name = "lstCust";
-            this.lstCust.Size = new System.Drawing.Size(366, 186);
+            this.lstCust.Size = new System.Drawing.Size(369, 199);
             this.lstCust.TabIndex = 3;
             this.lstCust.SelectedValueChanged += new System.EventHandler(this.lstCustomers_SelectedValueChanged);
             // 
@@ -120,9 +119,11 @@ namespace Assignment
             this.grpViewCust.Controls.Add(this.btnSearch);
             this.grpViewCust.Controls.Add(this.btnNewCustomer);
             this.grpViewCust.Controls.Add(this.lstCust);
-            this.grpViewCust.Location = new System.Drawing.Point(12, 63);
+            this.grpViewCust.Location = new System.Drawing.Point(11, 50);
+            this.grpViewCust.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grpViewCust.Name = "grpViewCust";
-            this.grpViewCust.Size = new System.Drawing.Size(401, 385);
+            this.grpViewCust.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpViewCust.Size = new System.Drawing.Size(392, 375);
             this.grpViewCust.TabIndex = 6;
             this.grpViewCust.TabStop = false;
             this.grpViewCust.Text = "View Customers";
@@ -130,10 +131,9 @@ namespace Assignment
             // chkNotPaid
             // 
             this.chkNotPaid.AutoSize = true;
-            this.chkNotPaid.Location = new System.Drawing.Point(24, 102);
-            this.chkNotPaid.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.chkNotPaid.Location = new System.Drawing.Point(18, 83);
             this.chkNotPaid.Name = "chkNotPaid";
-            this.chkNotPaid.Size = new System.Drawing.Size(216, 21);
+            this.chkNotPaid.Size = new System.Drawing.Size(162, 17);
             this.chkNotPaid.TabIndex = 6;
             this.chkNotPaid.Text = "Outstanding Payment(s) Only";
             this.chkNotPaid.UseVisualStyleBackColor = true;
@@ -142,10 +142,9 @@ namespace Assignment
             // rdoEmail
             // 
             this.rdoEmail.AutoSize = true;
-            this.rdoEmail.Location = new System.Drawing.Point(272, 65);
-            this.rdoEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoEmail.Location = new System.Drawing.Point(204, 53);
             this.rdoEmail.Name = "rdoEmail";
-            this.rdoEmail.Size = new System.Drawing.Size(83, 21);
+            this.rdoEmail.Size = new System.Drawing.Size(65, 17);
             this.rdoEmail.TabIndex = 5;
             this.rdoEmail.Text = "By Email";
             this.rdoEmail.UseVisualStyleBackColor = true;
@@ -153,10 +152,9 @@ namespace Assignment
             // rdoUsername
             // 
             this.rdoUsername.AutoSize = true;
-            this.rdoUsername.Location = new System.Drawing.Point(147, 65);
-            this.rdoUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoUsername.Location = new System.Drawing.Point(110, 53);
             this.rdoUsername.Name = "rdoUsername";
-            this.rdoUsername.Size = new System.Drawing.Size(114, 21);
+            this.rdoUsername.Size = new System.Drawing.Size(88, 17);
             this.rdoUsername.TabIndex = 5;
             this.rdoUsername.Text = "By Username";
             this.rdoUsername.UseVisualStyleBackColor = true;
@@ -164,20 +162,18 @@ namespace Assignment
             // rdoFullName
             // 
             this.rdoFullName.AutoSize = true;
-            this.rdoFullName.Location = new System.Drawing.Point(23, 65);
-            this.rdoFullName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.rdoFullName.Location = new System.Drawing.Point(17, 53);
             this.rdoFullName.Name = "rdoFullName";
-            this.rdoFullName.Size = new System.Drawing.Size(112, 21);
+            this.rdoFullName.Size = new System.Drawing.Size(87, 17);
             this.rdoFullName.TabIndex = 5;
             this.rdoFullName.Text = "By Full Name";
             this.rdoFullName.UseVisualStyleBackColor = true;
             // 
             // btnRefersh
             // 
-            this.btnRefersh.Location = new System.Drawing.Point(421, 62);
-            this.btnRefersh.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnRefersh.Location = new System.Drawing.Point(316, 50);
             this.btnRefersh.Name = "btnRefersh";
-            this.btnRefersh.Size = new System.Drawing.Size(93, 28);
+            this.btnRefersh.Size = new System.Drawing.Size(70, 23);
             this.btnRefersh.TabIndex = 2;
             this.btnRefersh.Text = "Refresh";
             this.btnRefersh.UseVisualStyleBackColor = true;
@@ -185,10 +181,9 @@ namespace Assignment
             // 
             // btnReset
             // 
-            this.btnReset.Location = new System.Drawing.Point(421, 25);
-            this.btnReset.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnReset.Location = new System.Drawing.Point(316, 20);
             this.btnReset.Name = "btnReset";
-            this.btnReset.Size = new System.Drawing.Size(93, 27);
+            this.btnReset.Size = new System.Drawing.Size(70, 22);
             this.btnReset.TabIndex = 2;
             this.btnReset.Text = "Reset";
             this.btnReset.UseVisualStyleBackColor = true;
@@ -196,10 +191,9 @@ namespace Assignment
             // 
             // btnNewCustomer
             // 
-            this.btnNewCustomer.Location = new System.Drawing.Point(23, 415);
-            this.btnNewCustomer.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnNewCustomer.Location = new System.Drawing.Point(13, 326);
             this.btnNewCustomer.Name = "btnNewCustomer";
-            this.btnNewCustomer.Size = new System.Drawing.Size(492, 37);
+            this.btnNewCustomer.Size = new System.Drawing.Size(369, 30);
             this.btnNewCustomer.TabIndex = 4;
             this.btnNewCustomer.Text = "New Customer";
             this.btnNewCustomer.UseVisualStyleBackColor = true;
@@ -209,19 +203,17 @@ namespace Assignment
             // 
             this.lblTitle.AutoSize = true;
             this.lblTitle.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.lblTitle.Location = new System.Drawing.Point(16, 27);
-            this.lblTitle.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblTitle.Location = new System.Drawing.Point(12, 18);
             this.lblTitle.Name = "lblTitle";
-            this.lblTitle.Size = new System.Drawing.Size(175, 25);
+            this.lblTitle.Size = new System.Drawing.Size(145, 20);
             this.lblTitle.TabIndex = 7;
             this.lblTitle.Text = "Receptionist Home";
             // 
             // btnService
             // 
-            this.btnService.Location = new System.Drawing.Point(12, 370);
-            this.btnService.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnService.Location = new System.Drawing.Point(9, 290);
             this.btnService.Name = "btnService";
-            this.btnService.Size = new System.Drawing.Size(275, 37);
+            this.btnService.Size = new System.Drawing.Size(206, 30);
             this.btnService.TabIndex = 10;
             this.btnService.Text = "Add Service";
             this.btnService.UseVisualStyleBackColor = true;
@@ -229,10 +221,9 @@ namespace Assignment
             // 
             // btnViewPayment
             // 
-            this.btnViewPayment.Location = new System.Drawing.Point(12, 415);
-            this.btnViewPayment.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.btnViewPayment.Location = new System.Drawing.Point(10, 326);
             this.btnViewPayment.Name = "btnViewPayment";
-            this.btnViewPayment.Size = new System.Drawing.Size(275, 37);
+            this.btnViewPayment.Size = new System.Drawing.Size(206, 30);
             this.btnViewPayment.TabIndex = 11;
             this.btnViewPayment.Text = "View Payment and Receipt";
             this.btnViewPayment.UseVisualStyleBackColor = true;
@@ -251,9 +242,11 @@ namespace Assignment
             this.grpSelectedCust.Controls.Add(this.lblPaymentStatus);
             this.grpSelectedCust.Controls.Add(this.lblUsername);
             this.grpSelectedCust.Controls.Add(this.lblName);
-            this.grpSelectedCust.Location = new System.Drawing.Point(432, 63);
+            this.grpSelectedCust.Location = new System.Drawing.Point(414, 50);
+            this.grpSelectedCust.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.grpSelectedCust.Name = "grpSelectedCust";
-            this.grpSelectedCust.Size = new System.Drawing.Size(221, 385);
+            this.grpSelectedCust.Padding = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.grpSelectedCust.Size = new System.Drawing.Size(228, 375);
             this.grpSelectedCust.TabIndex = 8;
             this.grpSelectedCust.TabStop = false;
             this.grpSelectedCust.Text = "Selected Customer";
@@ -261,47 +254,42 @@ namespace Assignment
             // txtPhoneNo
             // 
             this.txtPhoneNo.Enabled = false;
-            this.txtPhoneNo.Location = new System.Drawing.Point(12, 193);
-            this.txtPhoneNo.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtPhoneNo.Location = new System.Drawing.Point(9, 157);
             this.txtPhoneNo.Name = "txtPhoneNo";
-            this.txtPhoneNo.Size = new System.Drawing.Size(273, 22);
+            this.txtPhoneNo.Size = new System.Drawing.Size(206, 20);
             this.txtPhoneNo.TabIndex = 12;
             // 
             // txtEmail
             // 
             this.txtEmail.Enabled = false;
-            this.txtEmail.Location = new System.Drawing.Point(12, 143);
-            this.txtEmail.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtEmail.Location = new System.Drawing.Point(9, 116);
             this.txtEmail.Name = "txtEmail";
-            this.txtEmail.Size = new System.Drawing.Size(273, 22);
+            this.txtEmail.Size = new System.Drawing.Size(206, 20);
             this.txtEmail.TabIndex = 12;
             // 
             // txtUsername
             // 
             this.txtUsername.Enabled = false;
-            this.txtUsername.Location = new System.Drawing.Point(12, 95);
-            this.txtUsername.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtUsername.Location = new System.Drawing.Point(9, 77);
             this.txtUsername.Name = "txtUsername";
-            this.txtUsername.Size = new System.Drawing.Size(273, 22);
+            this.txtUsername.Size = new System.Drawing.Size(206, 20);
             this.txtUsername.TabIndex = 12;
             // 
             // txtFullName
             // 
             this.txtFullName.Enabled = false;
-            this.txtFullName.Location = new System.Drawing.Point(12, 44);
-            this.txtFullName.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
+            this.txtFullName.Location = new System.Drawing.Point(9, 36);
             this.txtFullName.Name = "txtFullName";
-            this.txtFullName.Size = new System.Drawing.Size(273, 22);
+            this.txtFullName.Size = new System.Drawing.Size(206, 20);
             this.txtFullName.TabIndex = 12;
             // 
             // lblEmail
             // 
             this.lblEmail.AutoSize = true;
             this.lblEmail.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblEmail.Location = new System.Drawing.Point(8, 123);
-            this.lblEmail.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblEmail.Location = new System.Drawing.Point(6, 100);
             this.lblEmail.Name = "lblEmail";
-            this.lblEmail.Size = new System.Drawing.Size(42, 17);
+            this.lblEmail.Size = new System.Drawing.Size(32, 13);
             this.lblEmail.TabIndex = 0;
             this.lblEmail.Text = "Email";
             // 
@@ -309,10 +297,9 @@ namespace Assignment
             // 
             this.lblPhoneNo.AutoSize = true;
             this.lblPhoneNo.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblPhoneNo.Location = new System.Drawing.Point(8, 174);
-            this.lblPhoneNo.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblPhoneNo.Location = new System.Drawing.Point(6, 141);
             this.lblPhoneNo.Name = "lblPhoneNo";
-            this.lblPhoneNo.Size = new System.Drawing.Size(107, 17);
+            this.lblPhoneNo.Size = new System.Drawing.Size(81, 13);
             this.lblPhoneNo.TabIndex = 0;
             this.lblPhoneNo.Text = "Phone Number:";
             // 
@@ -320,7 +307,8 @@ namespace Assignment
             // 
             this.lblPaymentStatus.AutoSize = true;
             this.lblPaymentStatus.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblPaymentStatus.Location = new System.Drawing.Point(6, 191);
+            this.lblPaymentStatus.Location = new System.Drawing.Point(6, 202);
+            this.lblPaymentStatus.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
             this.lblPaymentStatus.Name = "lblPaymentStatus";
             this.lblPaymentStatus.Size = new System.Drawing.Size(54, 13);
             this.lblPaymentStatus.TabIndex = 0;
@@ -330,10 +318,9 @@ namespace Assignment
             // 
             this.lblUsername.AutoSize = true;
             this.lblUsername.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblUsername.Location = new System.Drawing.Point(8, 75);
-            this.lblUsername.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblUsername.Location = new System.Drawing.Point(6, 61);
             this.lblUsername.Name = "lblUsername";
-            this.lblUsername.Size = new System.Drawing.Size(73, 17);
+            this.lblUsername.Size = new System.Drawing.Size(55, 13);
             this.lblUsername.TabIndex = 0;
             this.lblUsername.Text = "Username";
             // 
@@ -341,25 +328,23 @@ namespace Assignment
             // 
             this.lblName.AutoSize = true;
             this.lblName.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F);
-            this.lblName.Location = new System.Drawing.Point(8, 25);
-            this.lblName.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.lblName.Location = new System.Drawing.Point(6, 20);
             this.lblName.Name = "lblName";
-            this.lblName.Size = new System.Drawing.Size(71, 17);
+            this.lblName.Size = new System.Drawing.Size(54, 13);
             this.lblName.TabIndex = 0;
             this.lblName.Text = "Full Name";
             // 
             // FormRHome
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(677, 470);
+            this.ClientSize = new System.Drawing.Size(654, 436);
             this.Controls.Add(this.grpSelectedCust);
             this.Controls.Add(this.lblTitle);
             this.Controls.Add(this.grpViewCust);
             this.Controls.Add(this.btnUpdateProfile);
             this.Controls.Add(this.btnLogOut);
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.Name = "FormRHome";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Receptionist Home";
